070abd7ec13f669bb6246caa7b14f347711587fd
Michal Heczko <michal.heczko.ext@eng-sl.cz>
2022-03-30 15:35:34 +0200

    Initial commit

---
+++ src/main/java/com/example/bcgdvcodingchallenge/BcgdvCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bcgdvcodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcgdvCodingChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcgdvCodingChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bcgdvcodingchallenge/BcgdvCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bcgdvcodingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcgdvCodingChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
